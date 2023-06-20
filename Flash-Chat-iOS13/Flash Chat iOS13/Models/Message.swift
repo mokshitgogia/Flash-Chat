76ba72200a3961738c0665785b884a83be45aadc
MOKSHIT GOGIA <63094762+mokshitgogia@users.noreply.github.com>
2023-06-20 15:44:44 +0530

    Sync Github with local changes

---
+++ Flash-Chat-iOS13/Flash Chat iOS13/Models/Message.swift
@@ -0,0 +1,13 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Mokshit Gogia on 03/02/23.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import Foundation
+struct Message{
+    let sender:String
+    let body:String
+}
